0f87c2d8a5eae38db17c4fc9a765fb21d83ef51e
Ashmeet Singh <ashmeetsingh2010@gmail.com>
2020-12-27 20:34:03 -0500

    first push

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
 $(document).ready(function () {
     //Creating step boxes with unquie IDs
     for (var i = 0; i < 18; i++) {
-        $(".box1").append('<div class="step" onclick="posNum(this)" id="b' + i + '"></div>');
+        $(".box1").append('<div class="step" id="b' + i + '"></div>');
 
-        $(".box2").prepend('<div class="step2" onclick="posNum(this)" id="y' + i + '"></div>');
+        $(".box2").prepend('<div class="step2"  id="y' + i + '"></div>');
 
-        $(".box3").prepend('<div class="step" onclick="posNum(this)"  id="g' + i + '"></div>');
+        $(".box3").prepend('<div class="step"   id="g' + i + '"></div>');
 
-        $(".box4").append('<div class="step2" onclick="posNum(this)" id="r' + i + '"></div>');
+        $(".box4").append('<div class="step2"  id="r' + i + '"></div>');
+
     }
+    $(".redtoken").click(function () {
+        $(this).removeClass("redtoken")
+
+        $("#r5").addClass("redtoken")
+    });
+    $(".bluetoken").click(function () {
+        $(this).removeClass("redtoken")
+
+        $("#b5").addClass("redtoken")
+    });
 });
 
-function posNum(e) {
+/*function posNum(e) {
     //Getting the ID of the clicked step.
     var pos = $(e).attr("id");
     $()
@@ -23,7 +34,7 @@ function posNum(e) {
     // $("#" + pos).css("background-color", "red");
 
 
-}
+}*/
 
 function randomNumber() {
     //Generating a random dice roll value.
@@ -49,5 +60,53 @@ function randomNumber() {
         case 6:
             $(".diceImg").attr("src", "img/d6.png");
             break;
+    }
+
+    /*IF randnum is = 6 then */
+    if (randnum == 6) {
+        $(".innerbox").each(function () {
+            if ($(this).hasClass("thisUserTurn")) {
+
+                console.log($(this).attr("class"))
+                alert("Please (" +
+                    $(this).attr("class").split(" ")[0] + ")  move you token ")
+            }
+
+        })
+
+    } else {
+        $(".innerbox").each(function (index) {
+            console.log($(this).hasClass("thisUserTurn"))
+            if ($(this).hasClass("thisUserTurn")) {
+
+                $(this).removeClass("thisUserTurn")
+
+                var index = $(this).index(); //3
+                var nextIndex = index + 1; //4
+
+                /*console.log(index)
+console.log(nextIndex)*/
+
+                if (nextIndex < 4) {
+                    $(".innerbox").each(function (index) {
+                        if ($(this).index() == nextIndex) {
+                            $(this).addClass("thisUserTurn")
+                            return false;
+                        }
+                    })
+                } else {
+                    $(".innerbox").each(function (index) {
+                        if ($(this).index() == 0) {
+                            $(this).addClass("thisUserTurn")
+                            return false;
+                        }
+                    })
+                }
+
+                return false;
+            }
+        })
+
+
     }
 }
